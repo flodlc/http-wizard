c2202436177a300a68c2e7dc15e16181468f2595
Florian <delacomble@MacBook-Pro-de-Florian.local>
2023-11-04 23:43:27 +0100

    feat: changed api

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,2 @@
 export type { OkResponse } from "./clientFactory";
 export { createClient, createRoute } from "./proxyFactory";
-export type { Type } from "@sinclair/typebox";
-export { z } from "zod";
