03d5d115674855055db353e54545c6893857791c
Florian <delacomble@mbp-de-florian.home>
2023-11-08 22:51:04 +0100

    feat: export createQueryClient

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export { createQueryClient } from "./ReactQuery";
 export type { RouteDefinition } from "./types";
 export type { OkResponse } from "./clientFactory";
 export { createClient, createRoute } from "./proxyFactory";
